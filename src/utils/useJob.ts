c58e3c598cfc92be60e4b696422d412d486278d9
Julian Derungs <julian.derungs@deepbase.ch>
2024-02-05 12:13:08 +0100

    feat: add job utils to handle sync and async tasks sequentially
    relates to: WISE-136

---
+++ src/utils/useJob.ts
@@ -0,0 +1,53 @@
+import type { Log } from './useLogger'
+
+export interface ITask<TReturnType> {
+  start: <TData, TOptions>(data: TData, options?: TOptions, log?: Log) => TReturnType
+}
+
+export interface SyncTask<TReturnType> extends ITask<TReturnType> {}
+
+export interface AsyncTask<TReturnType> extends ITask<Promise<TReturnType>> {}
+
+export type Task<TReturnType> = AsyncTask<TReturnType> | SyncTask<TReturnType>
+
+export interface Job<TReturnType> extends AsyncTask<TReturnType> {
+  add: <T extends TReturnType>(...tasks: Task<T>[]) => void
+}
+
+export function isAsyncTask<TReturnType>(task: Task<TReturnType>): task is AsyncTask<TReturnType> {
+  // eslint-disable-next-line ts/no-unsafe-member-access
+  return 'start' in task && task.start instanceof Function && (task.start as any)[Symbol.toStringTag] === 'AsyncFunction'
+}
+
+export function isSyncTask<TReturnType>(task: Task<TReturnType>): task is SyncTask<TReturnType> {
+  // eslint-disable-next-line ts/no-unsafe-member-access
+  return 'start' in task && task.start instanceof Function && (task.start as any)[Symbol.toStringTag] !== 'AsyncFunction'
+}
+
+export function useJob<TReturnType>(): Job<TReturnType> {
+  const queue: Task<TReturnType>[] = [] as Task<TReturnType>[]
+
+  const add = <T extends TReturnType>(...tasks: Task<T>[]) => {
+    queue.push(...tasks)
+  }
+
+  const start = async <TData, TOptions>(data: TData, options?: TOptions, log?: Log): Promise<TReturnType> => {
+    let result: TData | Awaited<TReturnType> | undefined = data
+
+    // run all tasks sequentially
+    // sync and async ones
+    for (const task of queue) {
+      if (isAsyncTask(task))
+        result = await task.start(result, options, log)
+      else
+        result = await Promise.resolve(task.start(result, options, log))
+    }
+
+    return result as TReturnType
+  }
+
+  return {
+    add,
+    start,
+  }
+}
